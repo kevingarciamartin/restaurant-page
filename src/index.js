4ee4070d983c5282840530f84ec7013a80e0a159
Kevin Garcia Martin <kevingm@live.se>
2024-10-19 10:48:09 +0200

    Remove about page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./assets/styles/styles.css";
 import Index from "./assets/pages/indexContent";
 import Menu from "./assets/pages/menu";
 import Reservation from "./assets/pages/reservation";
-import About from "./assets/pages/about";
 import Contact from "./assets/pages/contact";
 
 Index();
@@ -26,8 +25,6 @@ const handleClickEvent = (() => {
         Menu();
       } else if (target.id === "nav-reservation") {
         Reservation();
-      } else if (target.id === "nav-about") {
-        About();
       } else if (target.id === "nav-contact") {
         Contact();
       }
